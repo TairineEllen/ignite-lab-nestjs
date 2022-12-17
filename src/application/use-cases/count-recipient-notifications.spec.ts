1217cfb2a0460d68728c5c639844d5c9bf13bd92
TairineEllen <tairine.ellen@gmail.com>
2022-12-17 16:11:03 -0300

    create make notification factory

--- src/application/use-cases/count-recipient-notifications.spec.ts
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -1,39 +1,20 @@
-import { Content } from "@application/entities/notification-content";
+import { makeNotification } from "@test/factories/notification-factory";
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
-import { Notification, NotificationProps } from "../entities/notification";
-import { CancelNotification } from './cancel-notification';
 import { CountRecipientNotifications } from "./count-recipient-notifications";
-import { NotificationNotFound } from "./errors/notification-not-found";
 
 describe('Count recipients notifications', () => {
   it('should be able to count recipients notifications', async () => {
     const repository = new InMemoryNotificationsRepository()
     const countRecipientNotifications = new CountRecipientNotifications(repository);
   
-    await repository.create(new Notification({
-      category: 'social',
-      content: new Content('Nova solicitação de amizade'),
-      recipientId: 'id-1'
-    }));
-
-    await repository.create(new Notification({
-      category: 'social',
-      content: new Content('Nova solicitação de amizade'),
-      recipientId: 'id-1'
-    }));
-
-    await repository.create(new Notification({
-      category: 'social',
-      content: new Content('Nova solicitação de amizade'),
-      recipientId: 'id-2'
-    }));
+    await repository.create(makeNotification({ recipientId: 'id-1'}));
+    await repository.create(makeNotification({ recipientId: 'id-1'}));
+    await repository.create(makeNotification({ recipientId: 'id-2'}));  
 
      const { count } = await countRecipientNotifications.execute({
       recipientId: 'id-1'
     });
   
     expect(count).toEqual(2);
-  });
-
-  
+  });  
 });
